05a9c0a919badde904319e5dfbfab87002079c84
Abolfazl Alijafari <134207588+Radisix@users.noreply.github.com>
2023-07-06 15:17:31 +0330

    LoginPageComplete

---
+++ Barbari_UI/Login.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Barbari_UI
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        public void RemoveText(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = "";
+                Username_Txt.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#000000"));
+            }
+        }
+        public void AddText(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+                Username_Txt.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#B8B8B8"));
+            }
+        }
+        private void PassWord_PassBx_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Password_PassBx.Password))
+            {
+                Password_PassBx.Password = Password_PassBx.Tag.ToString();
+                Password_PassBx.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#B8B8B8"));
+            }
+        }
+
+        private void PassWord_PassBx_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+            if (Password_PassBx.Password == Password_PassBx.Tag.ToString())
+            {
+                Password_PassBx.Password = "";
+                Password_PassBx.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#000000"));
+            }
+        }
+    }
+}
