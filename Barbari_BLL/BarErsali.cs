d9ca231f76a592e181de2839f438fd6601df2e3b
Abolfazl Alijafari <134207588+Radisix@users.noreply.github.com>
2023-07-10 20:56:09 +0330

    merge

--- Barbari_BLL/BarErsali.cs
+++ Barbari_BLL/BarErsali.cs
@@ -44,91 +44,10 @@ namespace Barbari_BLL
                 };
             }
         }
-<<<<<<< HEAD
-
-=======
-        public static OperationResult  Insert(BarErsali_Tbl barErsali,List<KalaDaryafti_Tbl> kalaDaryafti,bool moshtariSabet , bool maghsadNahayi)
+        public OperationResult Insert(BarErsali_Tbl barErsali)
         {
-            var result1 = Validation.BarErsali_Validation_EtelatFerestande(barErsali, moshtariSabet);
-            var result2 = Validation.BarErsali_Validation_EtelatGerande(barErsali, maghsadNahayi);
-            var result3 = Validation.BarErsali_Validation_EtelatBar(barErsali);
-            if (result1.Success == false)
-            {
-                return new OperationResult
-                {
-                    Success = false,
-                    Message = result1.Message
-                };
-            }
-            else if (result2.Success == false)
-            {
-                return new OperationResult
-                {
-                    Success = false,
-                    Message = result2.Message
-                };
-            }
-            else if (result3.Success == false)
-            {
-                return new OperationResult
-                {
-                    Success = false,
-                    Message = result3.Message
-                };
-            }
-            else
-            {
-                var result = Barbari_DAL.BarErsali.Insert(barErsali, kalaDaryafti);
-                if (result.Success == true)
-                {
-                    return new OperationResult
-                    {
-                        Success = true,
-                    };
-                }
-                else
-                {
-                    return new OperationResult
-                    {
-                        Success = false,
-                        Message = "خطایی رخ داده است لطفا با پشتیبان تماس بگیرید"
-                    };
-                }
-            }
-        }
-        public static OperationResult Insert_TahvilBeRanande(BarErsali_Tbl barErsali)
-        {
-            var result1 = Validation.BarErsali_Validation_TahvilRanande(barErsali);
-            if (result1.Success == false)
-            {
-                return new OperationResult
-                {
-                    Success = false,
-                    Message = result1.Message
-                };
-            }
-            else
-            {
-                var result = Barbari_DAL.BarErsali.Insert_TavilBeRanande(barErsali);
-                if (result.Success == true)
-                {
-                    return new OperationResult
-                    {
-                        Success = true,
-                    };
-                }
-                else
-                {
-                    return new OperationResult
-                    {
-                        Success = false,
-                        Message = "خطایی رخ داده است لطفا با پشتیبان تماس بگیرید"
-                    };
-                }
 
-            }
         }
->>>>>>> 89c09f8648a42646ec160aaef2fab7e2fc9bb983
 
     }
 }
