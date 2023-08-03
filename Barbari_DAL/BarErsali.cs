4e99204971fbded6fcb072fccc21b0489d83bbed
Mstaheri <mstaheri.011@gmail.com>
2023-08-03 21:26:41 +0330

    new

--- Barbari_DAL/BarErsali.cs
+++ Barbari_DAL/BarErsali.cs
@@ -30,6 +30,26 @@ namespace Barbari_DAL
                 };
             }
         }
+        public static OperationResult<BarErsali_Tbl> Select_FirstBarname()
+        {
+            DataClassBarbariDataContext linq = new DataClassBarbariDataContext();
+            try
+            {
+                var query = linq.BarErsali_Tbls.FirstOrDefault();
+                return new OperationResult<BarErsali_Tbl>
+                {
+                    Success = true,
+                    Data = query
+                };
+            }
+            catch (Exception)
+            {
+                return new OperationResult<BarErsali_Tbl>
+                {
+                    Success = false
+                };
+            }
+        }
         public static OperationResult<List<KalaDaryafti_Tbl>> Select_KalaDaryafti(int codeBarname)
         {
             DataClassBarbariDataContext linq = new DataClassBarbariDataContext();
@@ -92,6 +112,9 @@ namespace Barbari_DAL
                 conn.Open();
                 int lastValue = Convert.ToInt32(cmd.ExecuteScalar());
                 conn.Close();
+
+                var query_Select_First = Select_FirstBarname();
+
                 if (lastValue != 1000)
                 {
                     return new OperationResult<int>
@@ -100,12 +123,20 @@ namespace Barbari_DAL
                         Data = lastValue
                     };
                 }
-                else
+                else if(query_Select_First.Data == null)
                 {
                     return new OperationResult<int>
                     {
                         Success = true,
                         Data = 999
+                    };
+                }
+                else
+                {
+                    return new OperationResult<int>
+                    {
+                        Success = true,
+                        Data = 1000
                     };
                 }
             }
